dbdf7b9eba9e2067abf35fc67a2ec923de2007d4
foak77 <emailtofred@gmail.com>
2020-07-19 13:44:19 -0400

    Updating scores and changing players

--- app.js
+++ app.js
@@ -11,15 +11,48 @@ GAME RULES:
 var scores, roundScore, activePlayer, dice;
 scores = [0, 0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
 // console.log(dice)
 
-document.querySelector("#current-" + activePlayer).textContent = dice;
+//document.querySelector("#current-" + activePlayer).textContent = dice;
 // document.querySelector("#current-" + activePlayer).innerHTML = "<em>" + dice + "<em>"
+//var x = document.querySelector("#score-1").textContent;
+// console.log(x);
 
-var x = document.querySelector("#score-1").textContent;
-console.log(x);
+document.querySelector(".dice").style.display = "none";
 
-document.querySelector(".dice").getElementsByClassName.display = "none";
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  // 1.random number
+  dice = Math.floor(Math.random() * 6) + 1;
+  //2.display the result
+  var diceDom = document.querySelector(".dice");
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + dice + ".png";
+  //3.update the round score if the rolled n isn't n 1
+  if (dice !== 1) {
+    //add score
+    roundScore += dice;
+    document.querySelector("#current-" + activePlayer).textContent = roundScore;
+  } else {
+    //next player
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    roundScore = 0;
+
+    document.getElementById("current-0").textContent = "0";
+    document.getElementById("current-1").textContent = "0";
+
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+
+    // document.querySelector(".player-0-panel").classList.remove("active");
+    // document.querySelector(".player-1-panel").classList.add("active");
+
+    document.querySelector("dice").dice.display = "none";
+  }
+});
